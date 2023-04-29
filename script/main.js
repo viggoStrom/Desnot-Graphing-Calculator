d87fcb4bec9223a3051e2b2b5c6e534a9d6fb18a
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-04-29 21:00:20 +0200

    added user input support

--- script/main.js
+++ script/main.js
@@ -2,14 +2,6 @@ document.getElementById("canvasWrapper").scrollLeft = 600
 document.getElementById("canvasWrapper").scrollTop = 350
 
 
-const equation = (event) => {
-    src = event.srcElement
-
-    if (src.type == "checkbox") {
-        // do stuff
-    }
-}
-
 const addEqSlot = () => {
     const wrapper = document.createElement("div")
     wrapper.classList.add("equationWrapper")
@@ -18,12 +10,12 @@ const addEqSlot = () => {
     checkbox.type = "checkbox"
     checkbox.checked = true
     checkbox.classList.add("activate")
-    checkbox.onchange = "equation(event)"
+    checkbox.setAttribute("onchange", "equation(event)")
 
     const text = document.createElement("input")
     text.type = "text"
     text.classList.add("equation")
-    text.onchange = "equation(event)"
+    text.setAttribute("onchange", "equation(event)")
 
     wrapper.appendChild(checkbox)
     wrapper.appendChild(text)
@@ -32,6 +24,10 @@ const addEqSlot = () => {
 
     document.querySelectorAll(".equation")[document.querySelectorAll(".equation").length - 1].focus()
 }
+
+addEqSlot()
+addEqSlot()
+document.querySelectorAll(".equation")[0].focus()
 
 class plane {
     constructor() {
@@ -51,15 +47,41 @@ class plane {
         this.xGridSpacing = 50
         this.yGridSpacing = 50
 
+        this.availableColors = ["red", "green", "blue"]
+        this.usedColors = []
+
+        this.equations = []
+
 
         document.addEventListener("click", event => {
             const xScroll = document.getElementById("canvasWrapper").scrollLeft
-            console.log(xScroll);
-
-            if (xScroll < 10 || xScroll > this.canvas.width - 100) {
-                canvas.width += 500
-            }
+            const yScroll = document.getElementById("canvasWrapper").scrollTop
+            console.log(xScroll, yScroll);
         })
+    }
+
+    factorial = (num) => {
+        if (num < 0)
+            return -1
+        else if (num == 0)
+            return 1
+        else {
+            return (num * f(num - 1))
+        }
+    }
+
+    pickRandomColor = () => {
+        let color = this.availableColors[Math.floor(Math.random() * this.availableColors.length)];
+        let i = 0
+
+        while (this.usedColors.includes(color)) {
+            color = this.availableColors[Math.floor(Math.random() * this.availableColors.length)];
+            i++
+            if (i > this.availableColors.length * 4) { break }
+        }
+
+        this.usedColors.push(color)
+        return color
     }
 
     drawAxes = () => {
@@ -81,18 +103,18 @@ class plane {
 
     drawNum = () => {
         this.ctx.fillStyle = "black"
-        this.ctx.textAlign = "center"
+        this.ctx.textAlign = "left"
         this.ctx.font = "15px Arial"
 
         for (let index = this.xCenter - this.xGridSpacing * Math.floor(this.xCenter / this.xGridSpacing); index < this.width; index += this.xGridSpacing) {
             const x = index
-            const y = this.yCenter - 10
+            const y = this.yCenter - 8
 
             this.ctx.fillText(((index - this.xCenter) / this.xGridSpacing).toFixed(0), x, y)
         }
 
         for (let index = this.yCenter - this.yGridSpacing * Math.floor(this.yCenter / this.yGridSpacing); index < this.height; index += this.yGridSpacing) {
-            const x = this.xCenter + 10
+            const x = this.xCenter + 8
             const y = index
 
             this.ctx.fillText(((this.yCenter - index) / this.yGridSpacing).toFixed(0), x, y)
@@ -119,15 +141,28 @@ class plane {
         }
     }
 
-    drawFunc = () => {
-        this.ctx.strokeStyle = "red"
+    drawFunc = (func) => {
+        // func = func.replace(/^[^0-9xX]*([0-9]+|xX)[^0-9xX]*|[^\d()+\-*\/%!^xX]/g, "") // add = and equation type later
+        func = func.replace(/[^xX0-9+\-*/^.!()%]|([+\-*/^.%]+$)/g, "") // add = and equation type later
+        // func = func.replace(/^[^xX0-9+\-*/^.!%()]*(-?(?<![\-+*/(^])xX|\d+\.?\d*)(?![\d(])[^xX0-9+\-*/^.!()%]*|[^\d()+\-*\/%!^xX]/g, "") // add = and equation type later
+
+
+        this.ctx.strokeStyle = this.pickRandomColor()
         this.ctx.lineWidth = 3
 
         const y = (x) => {
             let c = this.xGridSpacing
-            return -Math.sin(x / c) * this.yGridSpacing
-            return -((x ** 2) / c - 4 * c)
-            return -(0.0002 * x ** 4 - .4 * x ** 2 + 3 * this.yGridSpacing)
+            x = x / c
+
+            try {
+                -eval(func) * c
+            } catch (error) {
+                console.log(func);
+                return console.log("Invalid function");
+            }
+
+            this.equations.push(func)
+            return -eval(func) * c
         }
 
         this.ctx.beginPath()
@@ -138,11 +173,34 @@ class plane {
         this.ctx.stroke()
         this.ctx.closePath()
     }
+
+    getFieldData = (index) => {
+        return document.getElementsByClassName("equation")[index].value
+    }
 }
+
+const equation = (event) => {
+    src = event.srcElement
+    console.log("object");
+
+    if (src.type == "checkbox") {
+        // do stuff
+    }
+    else if (src.type == "text") {
+        canvas.drawFunc(src.value)
+    }
+}
+
+// window.onbeforeunload = function (event) {
+//     return "string"
+// }
+
 
 const canvas = new plane()
 
 canvas.drawGrid()
 canvas.drawAxes()
 canvas.drawNum()
-canvas.drawFunc()
+
+// canvas.drawFunc("x**2-2")
+// canvas.drawFunc("x-1")
